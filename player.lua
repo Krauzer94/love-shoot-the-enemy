bdc03c1ef1e7f2d9dae9edfb0cd7545a4fc0381c
Krauzer94 <13894059+Krauzer94@users.noreply.github.com>
2025-07-25 13:55:31 -0300

    Enforce screen boundaries for player movement

--- player.lua
+++ player.lua
@@ -24,6 +24,16 @@ function Player:update(dt)
     elseif love.keyboard.isDown("right") then
         self.x = self.x + self.speed * dt
     end
+
+    -- Screen boundaries
+    local window_width = love.graphics.getWidth()
+
+    -- Enforce screen size
+    if self.x < 0 then
+        self.x = 0
+    elseif self.x + self.width > window_width then
+        self.x = window_width - self.width
+    end
 end
 
 function Player:draw()
