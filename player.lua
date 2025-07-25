43e9987b314fc4f846639dd0b3b70a9f9446090c
Krauzer94 <13894059+Krauzer94@users.noreply.github.com>
2025-07-25 13:52:49 -0300

    Basic player module

---
+++ player.lua
@@ -0,0 +1,23 @@
+Player = Object:extend()
+
+function Player:new()
+    -- Shooting player
+    self.image = love.graphics.newImage("images/panda.png")
+
+    -- Scalling factor
+    self.scale = 0.25
+
+    -- Apply new scaling
+    self.width = self.image:getWidth() * self.scale
+    self.height = self.image:getHeight() * self.scale
+
+    -- Player movement
+    self.x = 300
+    self.y = 20
+    self.speed = 500
+end
+
+function Player:draw()
+    -- On-screen rendering
+    love.graphics.draw(self.image, self.x, self.y, 0, self.scale, self.scale)
+end
