fd165b22e4144f3ef76c1fe17c9b7fc05018abac
Krauzer94 <13894059+Krauzer94@users.noreply.github.com>
2025-07-25 14:00:15 -0300

    Enforce screen boundaries for the Enemy element

--- enemy.lua
+++ enemy.lua
@@ -17,6 +17,22 @@ function Enemy:new()
     self.speed = 100
 end
 
+function Enemy:update(dt)
+    self.x = self.x + self.speed * dt
+
+    -- Screen boundaries
+    local window_width = love.graphics.getWidth()
+
+    -- Enforce screen size
+    if self.x < 0 then
+        self.x = 0
+        self.speed = -self.speed
+    elseif self.x + self.width > window_width then
+        self.x = window_width - self.width
+        self.speed = -self.speed
+    end
+end
+
 function Enemy:draw()
     love.graphics.draw(self.image, self.x, self.y, 0, self.scale, self.scale)
 end
